be291feb33cdbb434bcd5d76630f4b78a44c45e6
mahiro0312 <newchihiro123@yahoo.ne.jp>
2023-11-15 10:08:54 +0900

    座標上のアメの最大数を求める(失敗)

---
+++ practice_2023/10_28_C.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+static const int N_MAX = 3000000, M_MAX = 1000000000;
+
+int main(){
+    int N, M;
+    int A[M_MAX];
+
+    //読み取り
+    cin >> N >> M;
+
+    //for文でAの座標を読み取り
+    for(int i=0;i<N;i++){
+        cin >> A[i];
+    }
+    
+    //どう最大値を得るか
+
+    return 0;
+}
